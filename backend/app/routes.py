faf1fb76b5ae7ecbb7146adc0926a4293c5cee17
Sorin <sorin78910@gmail.com>
2025-05-25 01:58:52 +0300

    backend workflow

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,16 +1,13 @@
 import requests
 import os
-from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+from flask import Blueprint, jsonify, request
 
 FLOCKX_API_KEY = os.getenv("flockx_api_key")
-SMARTSHELF_AI_ID = "4b1fbb8e-ed44-4a88-9f63-b54107847a3c"
-
-app = Flask(__name__)
-CORS(app)
 
 
-@app.route('/agent-prompt', methods=['POST'])
+main = Blueprint("main", __name__)
+
+@main.route('/agent-prompt', methods=['POST'])
 def agent_page():
     if request.method == 'POST':
         data = request.get_json()
@@ -41,23 +38,3 @@ def agent_page():
             return jsonify({"error": f"Network Error: {str(e)}"}), 500
         except ValueError:
             return jsonify({"error": "Response is not valid JSON", "raw": r.text}), 500
-
-@app.route('/home')
-def index():
-    print(FLOCKX_API_KEY)
-    response = requests.post(
-        "https://api.flockx.io/api/v1/agents/4b1fbb8e-ed44-4a88-9f63-b54107847a3c/prompt",
-        headers={
-            "Authorization": f'Token {FLOCKX_API_KEY}'
-        },
-        json={
-            'prompt': "What is the current status of the smart shelf AI agent?",
-        }
-    )
-    response = response.json()
-    print(response)
-    return render_template('index.html', response=response)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
